ad750fc0adefb0abdd932a70ecf4c2c8106cb9cd
chyyuu <chyyuu@users.noreply.github.com>
2024-04-27 01:19:20 +0000

    deploy: cab09a8da5eef6fee713ee9f6cc126bda8963cd8

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ output_file="oscourse.mm.md"
 cat os.mm.md > "$output_file"
 
 # Loop through lecture numbers 1 to 7
-for i in {1..7}; do
+for i in {1..8}; do
     # Concatenate main lecture file first
     cat "mindmap/lec$i/lec$i.mm.md" >> "$output_file"
     
